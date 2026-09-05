a46932b3f347d9e67add5810a9403b99deb987df
OmarAwadSaber <01004005001a@gmail.com>
2026-09-05 22:51:57 +0300

    implemented books , category models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    description = models.TextField()
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
