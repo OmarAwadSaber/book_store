b5050a34068f7da7465cafd667767ccf74efb5d5
OmarAwadSaber <01004005001a@gmail.com>
2026-09-05 23:08:13 +0300

    implemented books_list module

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import books_list
 urlpatterns = [
-    
+    path('', books_list, name="books_list"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def books_list(request):
+    books = Book.objects.all()
+    
+    return render(request, 'books/books_list.html' , {"books_list" : books})
