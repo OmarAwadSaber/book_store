1546059cf2790ef7514d236dc939accb9fdecee0
OmarAwadSaber <01004005001a@gmail.com>
2026-09-05 23:19:13 +0300

    implemented book_detail module

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import books_list
+from .views import books_list, book_detail
 urlpatterns = [
     path('', books_list, name="books_list"),
+    path('<int:book_id>/', book_detail, name="book_detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def books_list(request):
     books = Book.objects.all()
     
     return render(request, 'books/books_list.html' , {"books_list" : books})
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id = book_id)
+    return render(request, 'books/book_detail.html', {"book" : book})
